b09338a50fd64dae5008de15f1569d40aed2eac0
Mostafa Mahmoud El-sherbiniy <mostafa.elsherbini@ctx.ae>
2026-08-16 05:56:00 +0300

    fix(dashboard): #fe-[Layout] Fix responsive

--- src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
-import { TranslatePipe } from '@ngx-translate/core';
-import { AppBase } from '@core/bases/app-base.base';
+import { DrawerModule } from 'primeng/drawer';
 import { AtlasSidebarComponent } from '@shared/ui/components/atlas-sidebar/atlas-sidebar.component';
 import { AtlasNavbarComponent } from '@shared/ui/components/atlas-navbar/atlas-navbar.component';
 
 @Component({
   selector: 'atlas-dashboard-layout',
-  imports: [RouterOutlet, ToastModule, TranslatePipe, AtlasSidebarComponent, AtlasNavbarComponent],
+  imports: [RouterOutlet, ToastModule, DrawerModule, AtlasSidebarComponent, AtlasNavbarComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss',
 })
-export class DashboardLayout {}
+export class DashboardLayout {
+  visible: boolean = false;
+  toggleSidebar() {
+    this.visible = !this.visible;
+  }
+}
--- src/app/shared/ui/components/atlas-navbar/atlas-navbar.component.ts
+++ src/app/shared/ui/components/atlas-navbar/atlas-navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { AtlasLangSwitchComponent } from '@shared/ui/atoms/atlas-lang-switch/atlas-lang-switch.component';
 import { AtlasProfileComponent } from '@shared/ui/organisms/atlas-profile/atlas-profile.component';
 import { AtlasTenantsComponent } from '@shared/ui/organisms/atlas-tenants/atlas-tenants.component';
+import { AtlasButtonComponent } from '@shared/ui/atoms/atlas-button/atlas-button.component';
 
 @Component({
   selector: 'atlas-navbar',
-  imports: [AtlasLangSwitchComponent, AtlasProfileComponent, AtlasTenantsComponent],
+  imports: [
+    AtlasLangSwitchComponent,
+    AtlasProfileComponent,
+    AtlasTenantsComponent,
+    AtlasButtonComponent,
+  ],
   templateUrl: './atlas-navbar.component.html',
   styleUrl: './atlas-navbar.component.scss',
 })
-export class AtlasNavbarComponent {}
+export class AtlasNavbarComponent {
+  /**
+   * Emitter - Sidebar toggler
+   */
+  onSidebarTogglerClick = output<void>();
+}
